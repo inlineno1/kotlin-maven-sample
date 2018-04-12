b4d9a65711d47faf51e6631cc0c3cd757d67c756
James <inlineno1@gamil.com>
2018-04-12 15:12:38 +0900

    first commit

---
+++ src/main/java/com/Q/maven_kotlin/App.java
@@ -0,0 +1,13 @@
+package com.Q.maven_kotlin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
